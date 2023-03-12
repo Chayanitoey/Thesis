7b5125bd502e9f958e03a5461558bd92bbcaa8a8
ChayTha <61849099+ChayTha@users.noreply.github.com>
2023-03-12 16:02:17 -0400

    adding to deploy on Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 
@@ -9,7 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+			  include: ['/*'],
+			  exclude: ['<all>']
+			}
+		  }
+		)
 	},
 	// let svelte take care of autoprefixing styles
 	preprocess: preprocess({
