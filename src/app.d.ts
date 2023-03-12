8b9f0403d462251312611152ca0172f193826c89
ChayTha <61849099+ChayTha@users.noreply.github.com>
2023-03-12 17:30:23 -0400

    edit cloudflare config

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,21 +1,50 @@
+// // See https://kit.svelte.dev/docs/types#app
+// // for information about these interfaces
+// declare global {
+// 	namespace App {
+// 		// interface Error {}
+// 		interface Locals {}
+// 		interface PageData {}
+// 		interface Platform {
+// 			env: {
+// 				COUNTER: DurableObjectNamespace;
+// 				};
+// 				context: {
+// 				 waitUntil(promise: Promise<any>): void;
+// 				 };
+// 				 caches: CacheStorage & { default: Cache }
+// 		}
+// 	}
+// }
+
+// export async function post({ request, platform }) {
+// 	const counter = platform.env.COUNTER.idFromName("A");
+//   }
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
 		interface Locals {}
-		interface PageData {}
-		interface Platform {
+
+	    interface Platform {
 			env: {
-				COUNTER: DurableObjectNamespace;
-				};
-				context: {
-				 waitUntil(promise: Promise<any>): void;
-				 };
-				 caches: CacheStorage & { default: Cache }
+			COUNTER: DurableObjectNamespace;
+			};
+			context: {
+			waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+			}	
+			interface Session {}
+
+    		interface Stuff {}
 		}
-	}
 }
+
+export {};
 
 export async function post({ request, platform }) {
 	const counter = platform.env.COUNTER.idFromName("A");
