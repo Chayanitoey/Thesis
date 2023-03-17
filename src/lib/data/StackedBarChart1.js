e14e598145e0b5abe83bf3d9ba32443d8f94342f
ChayTha <61849099+ChayTha@users.noreply.github.com>
2023-03-17 00:45:29 -0400

    saving

--- src/lib/data/StackedBarChart1.js
+++ src/lib/data/StackedBarChart1.js
@@ -1,5 +1,5 @@
 export default [
-	{ id: 1, Material_Broken: 72, Seam_Broken: 49, Pilling: 37, Stains: 36, Miscoloured: 35, Lost_Shape: 42,
+	{ id: 1, Material_Broken: 158, Stains: 36, Miscoloured: 35, Lost_Shape: 42,
         Got_Tired_Of_it:39, Others:54 }
 ];
 
